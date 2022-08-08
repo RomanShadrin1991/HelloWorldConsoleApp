c29c2a2e88dc67a9d1e3b472030903fd3c3e2bc2
Roman Shadrin <roma.shadrin.1991@gmail.com>
2022-08-08 08:03:34 +0300

    Добавьте файлы проекта.

---
+++ HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWordlConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание #1
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
+
+            Console.WriteLine();
+
+            // Задание #2
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+            Console.ReadLine();
+        }
+    }
+}
